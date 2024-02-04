9be56664945cb46819bbb8c8f7e50803badc66db
Luis Correa <luisciber640@gmail.com>
2024-02-04 13:48:32 -0300

    chore: script to build for ios simulator

---
+++ scripts/build-ios-sim.sh
@@ -0,0 +1,11 @@
+rm -r build
+mkdir build
+cd build
+cmake -G Xcode .. \
+  -DLLAMA_BUILD_EXAMPLES=OFF \
+  -DLLAMA_BUILD_TESTS=OFF \
+  -DLLAMA_BUILD_SERVER=OFF \
+  -DCMAKE_SYSTEM_NAME=iOS \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=14.0 \
+  -DCMAKE_OSX_SYSROOT=iphonesimulator
+cmake --build . --config Release -j $(sysctl -n hw.logicalcpu)
